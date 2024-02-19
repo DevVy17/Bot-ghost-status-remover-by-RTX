a82ddd41892d20deb97787a9b3537a9c19797345
DevVy17 <112255687+DevVy17@users.noreply.github.com>
2024-02-19 19:03:08 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Where Gaming Dreams come true","Discover Budget-Friendly Gaming Deals"];
 
 
 let currentIndex = 0;
